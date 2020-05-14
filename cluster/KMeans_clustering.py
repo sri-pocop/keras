bd0b97dd9ce6fd2ca7d954b95e2b5c83583dcbae
srini <53095631+sri-pocop@users.noreply.github.com>
2020-05-14 13:14:50 +0530

    Add files via upload

---
+++ cluster/KMeans_clustering.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 18:17:58 2020
+
+@author: srinivasan.c
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import pandas as pd
+from collections import Counter
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+ss = StandardScaler()
+from sklearn.cluster import KMeans
+from sklearn import cluster
+
+dataset = pd.read_csv('data/image_features2.csv')
+data = dataset
+#x = dataset.iloc[:,1:23].values 
+x = dataset.iloc[:,1:23].values 
+summary = dataset.describe().transpose()
+from scipy import stats
+x = x[(np.abs(stats.zscore(x)) < 3 ).all(axis = 1)]
+x_t = dataset.iloc[:,0].values 
+#y=  dataset.iloc[:,4].values
+"""
+summary = dataset.describe().transpose()
+from scipy import stats
+x = x[(np.abs(stats.zscore(x)) < 3 ).all(axis = 1)]
+"""
+label_encoder = LabelEncoder()
+data.iloc[:,0] = label_encoder.fit_transform(data.iloc[:,0]).astype('float64')
+
+corr = data.corr()
+columns = np.full((corr.shape[0],), True, dtype=bool)
+for i in range(corr.shape[0]):
+    for j in range(i+1, corr.shape[0]):
+        if corr.iloc[i,j] >= 0.9:
+            if columns[j]:
+                columns[j] = False
+selected_columns = data.columns[columns]
+data = data[selected_columns]
+
+#selected_columns = selected_columns[1:].values
+import statsmodels.api as sm
+def backwardElimination(x, Y, sl, columns):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(Y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+                    columns = np.delete(columns, j)
+                    
+    regressor_OLS.summary()
+    return x, columns
+SL = 0.05
+data_modeled, selected_columns = backwardElimination(data.iloc[:,1:].values, data.iloc[:,0].values, SL, selected_columns)
+data = data[selected_columns]
+"""fig = plt.figure(figsize = (20, 25))
+j = 0
+for i in data.columns:
+    plt.subplot(6, 4, j+1)
+    j += 1
+    sns.distplot(data[i][result['diagnosis']==0], color='g', label = 'benign')
+    sns.distplot(data[i][result['diagnosis']==1], color='r', label = 'malignant')
+    plt.legend(loc='best')
+fig.suptitle('Breast Cance Data Analysis')
+fig.tight_layout()
+fig.subplots_adjust(top=0.95)
+plt.show()
+"""
+x_d = data_modeled
+wcss = []
+
+x_d = ss.fit_transform(x_d)
+k = 4
+kmeans = cluster.KMeans(n_clusters = k)
+kmeans.fit(x_d)
+
+
+#x_t.head(5)
+
+labels = kmeans.labels_
+ds = kmeans.cluster_centers_
+print(pd.crosstab(x_t,labels))
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+principalComponents = pca.fit_transform(x)
+principalDf = pd.DataFrame(data = principalComponents
+             , columns = ['principal component 1', 'principal component 2'])
+
+finalDf = pd.concat([principalDf, dataset[['SAMPLE']]], axis = 1)
+
+fig = plt.figure(figsize = (8,8))
+ax = fig.add_subplot(1,1,1) 
+ax.set_xlabel('Principal Component 1', fontsize = 15)
+ax.set_ylabel('Principal Component 2', fontsize = 15)
+ax.set_title('2 component PCA', fontsize = 20)
+targets = [0, 1, 2, 3]
+colors = ['r', 'g', 'b', 'y']
+for target, color in zip(targets,colors):
+    indicesToKeep = finalDf['SAMPLE'] == target
+    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+               , finalDf.loc[indicesToKeep, 'principal component 2']
+               , c = color
+               , s = 50)
+ax.legend(targets)
+ax.grid()
