605b24aa24d1c0e4b3ba8693f557dd896c1cf6c3
srini <53095631+sri-pocop@users.noreply.github.com>
2020-07-03 16:35:17 +0530

    Update 3dplot.py

--- KMeans/3dplot.py
+++ KMeans/3dplot.py
@@ -6,4 +6,8 @@ for i in range(0, len(finalDf)):
 threedee = plt.figure().gca(projection='3d')
 threedee.scatter(finalDf.index, finalDf['principal component 1'], finalDf['principal component 2']
                      , c = finalDf['color'])
+threedee.set_xlabel('index')
+threedee.set_ylabel('component1')
+threedee.set_zlabel('component2')
+plt.show()
 # 3D plot code ends
