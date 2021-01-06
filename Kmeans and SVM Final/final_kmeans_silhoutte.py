54c1b548fd1c1eda4203f51eaa97a4956fafa5dc
srini <53095631+sri-pocop@users.noreply.github.com>
2021-01-06 11:51:45 +0530

    Update final_kmeans_silhoutte.py

--- Kmeans and SVM Final/final_kmeans_silhoutte.py
+++ Kmeans and SVM Final/final_kmeans_silhoutte.py
@@ -1,15 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-put train data under : data/im_data.csv
-put test data under : data/pred_data.csv
-    
-pred_data.csv file must have "contr","energ","maxpr" colums
-
-The predicted class will be saved in next column once the program runs, 
-    with "pred_class" column name
-
-"""
-
 #Inporting Libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -22,6 +10,8 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import silhouette_score
 from sklearn.metrics import silhouette_samples, silhouette_score
 import matplotlib.cm as mcm
+import warnings
+warnings.filterwarnings("ignore")
 
 #importing Dataset and seperating Dependent and independent Variables
 dataset = pd.read_csv('data/im_data.csv')
@@ -39,46 +29,14 @@ k = 4
 
 #Defining KMeans Model using Train Data
 kmeans = cluster.KMeans(n_clusters = k)
-clf_ = kmeans.fit(x_train,y_train)
-cm_for_label = confusion_matrix(clf_.labels_,y_train)
-print(cm_for_label)
+kmeans.fit(x_train)
 
-#for getting Exact Labels
-original_labels = []
-for i in range(0,4):
-    max_val = 0
-    max_val_index = 0
-    for j in range(0,k):    
-        #print(i,j,cm_for_label[i][j])
-        if cm_for_label[i][j] > max_val:
-            max_val = cm_for_label[i][j]
-            max_val_index = j
-    original_labels.append(max_val_index)
-
-#For calculating accuracy
-def for_accuracy(y, y_pred):    
-    correct = 0
-    wrong = 0
-    total = len(y)
-    for i in range(0, total):
-        if label_encoder.classes_[int(y[i])] == label_encoder.classes_[original_labels[y_pred[i]]]:
-            correct = correct + 1
-        else:
-            wrong = wrong + 1       
-        #print(int(y[i]), original_labels[clf_.labels_[i]])  
-    return str(round((correct/total)*100, 2)) + ' %'
-print('Train Accuracy : ', for_accuracy(y_train, clf_.labels_))          
 #Predicting using Test Data
 y_pred = kmeans.predict(x_test)
-y_pred_classes = []
-for i in range(0, len(y_pred)):
-    y_pred_classes.append(label_encoder.classes_[original_labels[y_pred[i]]])
-y_pred_classes = np.array(y_pred_classes)
 
 #Visualizing Predicted data using confusion matrix
 cm = confusion_matrix(y_test,y_pred)
 print(cm)
-print('Test Accuracy : ', for_accuracy(y_test, y_pred))  
 
 # silhouette plot
 
@@ -187,8 +145,8 @@ finalDf = pd.concat([principalDf, dataset[['CLASS']]], axis = 1)
 
 fig = plt.figure(figsize = (8,8))
 ax = fig.add_subplot(1,1,1) 
-ax.set_xlabel('Component 1', fontsize = 15)
-ax.set_ylabel('Component 2', fontsize = 15)
+ax.set_xlabel('F 1', fontsize = 15)
+ax.set_ylabel('F 2', fontsize = 15)
 ax.set_title('2 component PCA', fontsize = 20)
 targets = [0, 1, 2, 3]
 colors = ['r', 'g', 'b', 'y']
@@ -200,10 +158,8 @@ for target, color in zip(targets,colors):
                , s = 50)
 ax.legend(targets)
 ax.grid()
-plt.show()
-#Plot Code Ends
 
-# For three 3d plots - Starts
+#Plot Code Ends# -*- coding: utf-8 -*-
 from mpl_toolkits import mplot3d
 from sklearn.preprocessing import StandardScaler
 s_s = StandardScaler()
@@ -232,4 +188,3 @@ threedee.scatter(for_3d_plot.index, for_3d_plot['p1'], for_3d_plot['p3']
 threedee.set_xlabel('index')
 threedee.set_ylabel('p1')
 threedee.set_zlabel('p3')
-# For three 3d plots - Ends
